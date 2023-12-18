dfe690dade36733dd9149693b0ece27703e9ddc6
Artur Matysiak <inz.matys@gmail.com>
2023-12-18 21:14:08 +0100

    task done

--- 01/app.js
+++ 01/app.js
@@ -1,21 +1,24 @@
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
+
+const divList = document.querySelectorAll("div");
 
 function init() {
-    const divList = document.querySelectorAll('div');
-    
-    setBorderColorAsync(divList[0], 'red', function() {
-        setBorderColorAsync(divList[1], 'blue', function() {
-            setBorderColorAsync(divList[2], 'green', function() {
-                console.log('finish');
-            });
-        });
-    });
-
+  return new Promise((resolve, reject) => {
+    resolve("jest ok");
+    reject("błąd");
+  });
 }
 
+init()
+  .then(setBorderColorAsync(divList[0], "red", function () {}))
+  .then(setBorderColorAsync(divList[1], "blue", function () {}))
+  .then(setBorderColorAsync(divList[2], "green", function () {}))
+  .then((resolve) => console.log(resolve))
+  .catch((reject) => console.log(reject));
+
 function setBorderColorAsync(element, color, callback) {
-    setTimeout(() => {
-        element.style.border = `3px solid ${color}`;
-        callback();
-    }, Math.random() * 3000);
+  setTimeout(() => {
+    element.style.border = `3px solid ${color}`;
+    callback();
+  }, Math.random() * 3000);
 }

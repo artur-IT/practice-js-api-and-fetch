ac700b628e7c53c08ae41cfce843bd9106a1640e
Artur Matysiak <inz.matys@gmail.com>
2023-12-19 19:04:03 +0100

    task done? hmm

--- 02/app.js
+++ 02/app.js
@@ -1,32 +1,28 @@
-document.addEventListener('DOMContentLoaded', init);
-
-function init() {
-    const divList = document.querySelectorAll('div');
-    
-    setBorderColorAsync(divList[0], 'red', function() {
-        setBorderColorAsync(divList[1], 'blue', function() {
-            setBorderColorAsync(divList[2], 'green', function() {
-                console.log('finish');
-            });
-        });
-    });
-
-}
+document.addEventListener("DOMContentLoaded", setBorderColorAsync);
+const divList = document.querySelectorAll("div");
 
 function setBorderColorAsync(element, color, callback) {
-    if(element && element instanceof HTMLElement) {
-        // sprawdzam czy parametr jest elementem DOM, więcej:
-        // https://stackoverflow.com/questions/384286/javascript-isdom-how-do-you-check-if-a-javascript-object-is-a-dom-object
-        
-        if(callback && typeof callback === 'function') {
-            setTimeout(() => {
-                element.style.border = `3px solid ${color}`;
-                callback();
-            }, Math.random() * 3000);
-        } else {
-            alert('Parametr ~callback~ mus być funkcją');
-        }
-    } else {
-        alert('Paremetr ~element~ musi być prawidłowym elementem DOM');
-    }
+  return new Promise((resolve, reject) => {
+    if (element && element instanceof HTMLElement) {
+      // sprawdzam czy parametr jest elementem DOM, więcej:
+      // https://stackoverflow.com/questions/384286/javascript-isdom-how-do-you-check-if-a-javascript-object-is-a-dom-object
+
+      if (callback && typeof callback === "function")
+        resolve(
+          setTimeout(() => {
+            element.style.border = `3px solid ${color}`;
+            callback();
+          }, Math.random() * 3000)
+        );
+      else reject("Parametr ~callback~ musi być funkcją");
+    } else reject("Paremetr ~element~ musi być prawidłowym elementem DOM");
+  });
 }
+
+setBorderColorAsync().then(
+  setBorderColorAsync(divList[0], "red", () => {
+    setBorderColorAsync(divList[1], "blue", () => {
+      setBorderColorAsync(divList[2], "green", () => {});
+    });
+  })
+);

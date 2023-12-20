cf7441297f022199494760a74fb2f8c70a9cc201
Artur Matysiak <inz.matys@gmail.com>
2023-12-20 11:35:22 +0100

    task done? hmm

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,17 @@
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
+const url = "https://api.ipify.org?format=json";
 
 function init() {
-    console.log('DOM');
+  document.querySelector("button").addEventListener("click", () => new MyIP().showIP());
+}
+
+class MyIP {
+  constructor() {
+    fetch(url)
+      .then((ip) => ip.json())
+      .then((ip) => this.showIP(ip.ip))
+      .catch((err) => console.log("Uppps, something's wrong!", err));
+  }
+
+  showIP = (ip) => (document.body.querySelector("span").textContent = ip);
 }
